c6c2865d691c3bebeaa7fa36efff1e4ad07cc42e
Marcus Paeschke <marcus@mphasize.de>
2017-05-25 13:33:43 +0200

    Example OAuth client as node server app

---
+++ index.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const express = require('express');
+const simpleOauthModule = require('simple-oauth2');
+
+const app = express();
+const oauth2 = simpleOauthModule.create({
+  client: {
+    id: 'example_server_app',
+    secret: 'example-server',
+  },
+  auth: {
+    tokenHost: 'http://localhost:8888',
+    tokenPath: '/api/v1/token',
+    authorizePath: '/api/v1/authorize',
+  },
+});
+
+// Authorization uri definition
+const authorizationUri = oauth2.authorizationCode.authorizeURL({
+  redirect_uri: 'http://localhost:3000/callback',
+  scope: 'workspaces',
+  state: '3(#0/!~',
+});
+
+// Initial page redirecting to Github
+app.get('/auth', (req, res) => {
+  console.log(authorizationUri);
+  res.redirect(authorizationUri);
+});
+
+// Callback service parsing the authorization token and asking for the access token
+app.get('/callback', (req, res) => {
+  const code = req.query.code;
+  const options = {
+    code,
+  };
+
+  oauth2.authorizationCode.getToken(options, (error, result) => {
+    if (error) {
+      console.error('Access Token Error', error.message);
+      return res.json('Authentication failed');
+    }
+
+    console.log('The resulting token: ', result);
+    const token = oauth2.accessToken.create(result);
+
+    return res
+      .status(200)
+      .json(token);
+  });
+});
+
+app.get('/success', (req, res) => {
+  res.send('');
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello<br><a href="/auth">Log in with Incom</a>');
+});
+
+app.listen(3000, () => {
+  console.log('Express server started on port 3000'); // eslint-disable-line
+});
+
+
+// Credits to [@lazybean](https://github.com/lazybean)
